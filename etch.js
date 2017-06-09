1a4e9140a44be2b253e1d709cb4a9837e0a2d257
Nam Doan <nam.quang.doan@gmail.com>
2017-06-09 18:44:43 -0400

    Updated for size button

--- etch.js
+++ etch.js
@@ -1,15 +1,38 @@
 
 $(document).ready (function () {
-	var e = $(' <div class = "fluff"> </div>');
-	for (i = 0; i< 198; i++){
-		$('#container').append(e.clone());
-	}
+	newBoard (8);
 
-	$('.fluff').hover (function () {
-		$(this).css('background-color', 'red');
+	$('#size').click (function () {
+		var temp = $('#new_size').val();
+		newBoard(temp);
+
+		$('#new_size').val("");
 	});
+
+	change ();
 	
 	$('#reset').click (function () {
 		$('.fluff').css('background-color', 'grey');
 	});
+
+	
 });
+
+function newBoard (num) {
+	$('#container').empty();
+	var temp = (790 / num);
+	var e = $(' <div class = "fluff"> </div>');
+	$(e).css ('width', temp);
+	$(e).css ('height', temp);
+	for (i = 0; i< num*num; i++){
+		$('#container').append(e.clone());
+	}
+
+	change ();
+}
+
+function change () {
+	$('.fluff').hover (function () {
+		$(this).css('background-color', 'brown');
+	});
+}
